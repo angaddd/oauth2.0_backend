f83272f3954f05d8596a8df89f52d65c51e46c5b
Angad <angadkaur1015@gmail.com>
2026-04-04 23:04:56 +0530

    settings updated

--- backend/urls.py
+++ backend/urls.py
@@ -3,6 +3,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('accounts.urls')),
+    path('api', include('accounts.urls')),
     path('oauth/', include('social_django.urls', namespace='social')),
 ]
